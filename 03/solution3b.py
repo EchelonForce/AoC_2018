4e1bd8ccecf9c3c20cc194f8a0a4fedf9bb12788
EchelonForce <jbookout@gmail.com>
2018-12-03 22:26:06 +0100

    Day 3 Solved
    https://adventofcode.com/2018/day/3

---
+++ 03/solution3b.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import re
+
+lines = []
+#with open( 'input_test.txt', 'r' ) as f:
+with open( 'input1.txt', 'r' ) as f:
+    for line in f:
+        lines.append(line.rstrip())
+
+pattern = '#(?P<id>[0-9]+) @ (?P<x>[0-9]+),(?P<y>[0-9]+): (?P<w>[0-9]+)x(?P<h>[0-9]+)'
+pat = re.compile(pattern)
+
+the_grid = []
+size = 1000
+for i in range(size):
+    row = []
+    for j in range(size):
+        row.append(0)
+    the_grid.append( row )
+
+#for r in the_grid:
+#    print r
+
+for l in lines:
+    m = pat.match(l)
+    
+    id = m.group('id')
+    x = int(m.group('x'))
+    y = int(m.group('y'))
+    w = int(m.group('w'))
+    h = int(m.group('h'))
+
+    for i in range( x, x+w ):
+        for j in range( y, y+h):
+            the_grid[i][j]+=1
+
+print( '-------------------------------')
+print( '-------------------------------')
+
+#for r in the_grid:
+#    print r
+
+for l in lines:
+    m = pat.match(l)
+    
+    id = m.group('id')
+    x = int(m.group('x'))
+    y = int(m.group('y'))
+    w = int(m.group('w'))
+    h = int(m.group('h'))
+
+    overlapped = False
+    for i in range( x, x+w ):
+        if overlapped:
+            break
+        for j in range( y, y+h):
+            if the_grid[i][j]!=1:
+                overlapped = True
+                break
+
+    if not overlapped:
+        print( '%s Doesn\'t Overlap!'%id )
+
+
+#overlaps=0
+#for row in the_grid:
+#    for val in row:
+#        if val > 1:
+#            overlaps+=1
+#
+#print( 'Overlaps: %d'%overlaps )
+ 
